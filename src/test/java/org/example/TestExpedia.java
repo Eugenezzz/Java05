545d9c76c2cbeaabad2371023fc1957970af08e5
Eugenezzz <zaycevea@gmail.com>
2022-10-30 03:33:55 -0400

    Add TestExpedia

--- src/test/java/org/example/TestExpedia.java
+++ src/test/java/org/example/TestExpedia.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class TestExpedia {
-    public static void main(String[] args) {
-        System.out.println("Hello github");
+    static {
+        WebDriverManager.chromedriver().setup();
+    }
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testExpedia() {
+        driver.get ("https://www.expedia.com/");
+        driver.findElement(By.xpath("//span[text()='Flights']\n")).click();
+        Assert.assertEquals(driver.getTitle(), "Expedia Travel: Vacation Homes, Hotels, "
+                + "Car Rentals, Flights & More");
     }
 }
+
